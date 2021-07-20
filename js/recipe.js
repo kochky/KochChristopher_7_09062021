333f1127f7eff7c4ef644d86bb106786c2d99e83
Koch Christopher <koch.christopher@hotmail.fr>
2021-07-20 16:53:06 +0200

    facto code

--- js/recipe.js
+++ js/recipe.js
@@ -18,22 +18,22 @@ class Recipe {
 
     renderCard(){//HTML pattern of the cards  
         return `
-        <div id=${this.id} class="card col-4">
-            <img class="card-img-op bg-dark" src="" alt="" height="178">
-            <div class="card-body bg-light">
-                <div class=row>
-                    <h5 class="card-title col-8">${this.name}</h5>
-                    <p class="card-title col-4 font-weight-bold "><i class="far fa-clock fa-lg"></i> ${this.time}min</p>
-                </div>
-                <div class=row>
-                    <div id=ingredients-${this.id} class="card-text line-height col-6">
+            <div id=${this.id} class="card col-4">
+                <img class="card-img-op bg-dark" src="" alt="" height="178">
+                <div class="card-body bg-light">
+                    <div class=row>
+                        <h5 class="card-title col-8">${this.name}</h5>
+                        <p class="card-title col-4 font-weight-bold "><i class="far fa-clock fa-lg"></i> ${this.time}min</p>
                     </div>
-                    <p class="col-6 card-text roboto">
-                        ${this.description}
-                    </p>
-                </div>
-            </div>         
-        </div> `
+                    <div class=row>
+                        <div id=ingredients-${this.id} class="card-text line-height col-6">
+                        </div>
+                        <p class="col-6 card-text roboto">
+                            ${this.description}
+                        </p>
+                    </div>
+                </div>         
+            </div> `
     }
 
     createCard(){ //Put the "renderCard" in the div "Grid"
@@ -41,20 +41,20 @@ class Recipe {
         cardDeck.innerHTML += this.renderCard()
     }
 
-    renderRecipe( i){
-        if((i.quantity) ==undefined){
-            return  `<p ><b> ${i.ingredient}</b></p>`  
+    renderRecipe( ingredients){
+        if((ingredients.quantity) ==undefined){
+            return  `<p ><b> ${ingredients.ingredient}</b></p>`  
         }
-       else if ((i.unit) !=undefined){
-       return  `<p ><b> ${i.ingredient}:</b> ${i.quantity} ${i.unit}</p>`
+       else if ((ingredients.unit) !=undefined){
+            return  `<p ><b> ${ingredients.ingredient}:</b> ${ingredients.quantity} ${ingredients.unit}</p>`
         }else {
-            return  `<p ><b> ${i.ingredient}:</b> ${i.quantity}</p>` 
+            return  `<p ><b> ${ingredients.ingredient}:</b> ${ingredients.quantity}</p>` 
         }
     }
 
     createIngredient(){
-        for (let i of this.ingredients){
-        document.getElementById("ingredients-"+this.id).innerHTML += this.renderRecipe(i);
+        for (let ingredient of this.ingredients){
+        document.getElementById("ingredients-"+this.id).innerHTML += this.renderRecipe(ingredient);
         }
     }
 }
